077358c299b1c068bc2dddc4be540838af145b1a
Himanshu Thakur <himanshu.thakur@digitalcareerinstitute.org>
2018-12-03 14:31:34 +0100

    first commit 2

--- libs/db-connection.js
+++ libs/db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/chat', {useMongoClient: true});
+mongoose.connect('mongodb://smartdigihub:1234567890abc@ds123844.mlab.com:23844/chat', {useMongoClient: true});
 
 mongoose.connection
   .once('open', () => console.log('Connected to the database'))
