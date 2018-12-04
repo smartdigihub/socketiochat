3527b4acbf46095ee08b2ffe71fcb421992b70a2
Himanshu Thakur <himanshu.thakur@digitalcareerinstitute.org>
2018-12-04 11:43:34 +0100

    changed css

--- public/main.js
+++ public/main.js
@@ -2,54 +2,62 @@ const socket = io.connect(location.href);
 
 // dom
 const message = document.querySelector('#message'),
-      handle = document.querySelector('#name'),
-      btn = document.querySelector('#send'),
-      output = document.querySelector('#output'),
-      chat = document.querySelector('#chat'),
-      feedback = document.querySelector('#feedback'),
-      clean = document.querySelector('#clean');
-      time = document.querySelector('#time');
+  handle = document.querySelector('#name'),
+  btn = document.querySelector('#send'),
+  output = document.querySelector('#output'),
+  chat = document.querySelector('#chat'),
+  feedback = document.querySelector('#feedback'),
+  clean = document.querySelector('#clean');
+time = document.querySelector('#time');
 
-      // event
-      btn.addEventListener('click', () => {
-        if (message.value != '' && handle.value != '') {
-          handle.style.backgroundColor = "gray";
-          handle.disabled = true;
+// event
+btn.addEventListener('click', () => {
+  if (message.value != '' && handle.value != '') {
+    handle.style.backgroundColor = "gray";
+    handle.disabled = true;
 
-          socket.emit('chat', {
-            message: message.value,
-            handle: handle.value
-          });
+    socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+    });
 
-        } else {
-          alert('All fields are required!');
-        }
-        message.value = '';
-      });
+  } else {
+    alert('All fields are required!');
+  }
+  message.value = '';
+});
 
-      function readURL(event){
-        var getImagePath = URL.createObjectURL(event.target.files[0]);
-        chat.style.backgroundImage = 'url(' + getImagePath + ')';
-      }
-      
-      message.addEventListener('keypress', () => {
-        socket.emit('typing', handle.value);
-      });
+function readURL(event) {
+  var getImagePath = URL.createObjectURL(event.target.files[0]);
+  chat.style.backgroundImage = 'url(' + getImagePath + ')';
+}
 
-      socket.on('chat', data => {
-        console.log(data);
-        output.innerHTML += `<p><strong>${data.handle}: </strong>${data.message}</p>`;
-        time.innerHTML += `<p><strong>${data.createdAt}</strong></p>`;
-      });
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
 
-      let timer = setTimeout(makeNoTypingState, 1000);
+socket.on('chat', data => {
+  console.log(data);
 
-      socket.on('typing', data => {
-        feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
-        clearTimeout(timer);
-        timer = setTimeout(makeNoTypingState, 1000);
-      });
-      
-      function makeNoTypingState() {
-        feedback.innerHTML = "";
-      }
+  chat.innerHTML += `<div class="msg">
+        <div class="output" id="output">
+        <strong>${data.name}: </strong>${data.message}   
+          </div>
+        <div class="time" id="time">
+        ${data.createdAt}
+        </div>
+        
+    </div>`;
+});
+
+let timer = setTimeout(makeNoTypingState, 1000);
+
+socket.on('typing', data => {
+  feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
+  clearTimeout(timer);
+  timer = setTimeout(makeNoTypingState, 1000);
+});
+
+function makeNoTypingState() {
+  feedback.innerHTML = "";
+}
