f3e096b1ea4eaa480346b49b31bd9e4e0f9a32d0
Himanshu Thakur <himanshu.thakur@digitalcareerinstitute.org>
2018-12-06 16:17:21 +0100

    Lots of changes

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
-// Deploy to git and Heroku steps:
+// Deploy to git and Heroku:
 // 1. Make your port dynamic
 // 2. Mention start script of your application in package.json
 // 3. Create a database in mlab (localhost won't work)
+
 // 4. Push it to git repository:
-// git init
-// add .gitignore
-// git add .
-// git commit -m "Message of file change"
-// git push -u origin master
+// - git init
+// - add .gitignore to your project
+// - git add .
+// - git commit -m "Message of file change"
+// - git push -u origin master
 
-// 5. Create app in heroku: heroku create
-// 6. Deploy git repository to heroku: git push heroku
+// 5. Push your working git repository to Heroku
+// - Create app in heroku: heroku create
+// - Deploy git repository to heroku: git push heroku
 
+//Include external and internal Libraries
+
+//Include Express and create app
 const express = require('express');
 const app = express();
-const ServerIO = require('socket.io');
-require('./libs/db-connection');
-var port = process.env.PORT || 8080;
 
-// listen
+//Include Socket.io library
+const ServerIO = require('socket.io');
+
+//Include connection to database
+const dbConnection = require('./libs/db-connection');
+
+//Include mongoose model
+const Chat = require('./models/Chat');
+
+// listen to port and start server
+var port = process.env.PORT || 8080;
 var server = app.listen(port, () => {
   console.log(`Server Running on ${port}`);
 });
 
+//Use SocketIO on server
 const io = ServerIO(server);
 
-
+//Middleware in Express
 app.use('/public', express.static('public'));
+
+//Set view engine (hbs, ejs, pug)
 app.set('view engine', 'ejs');
 
-const Chat = require('./models/Chat');
 
 app.get('/', (req, res) => {
   Chat.find({}).then(messages => {
@@ -37,14 +51,29 @@ app.get('/', (req, res) => {
   }).catch(err => console.error(err));
 });
 
+
+// Chatroom
+var numUsers = 0;
+
+// Establish connection with client
 io.on('connection', socket => {
+  var number = ++numUsers
+  console.log(`${number} user(s) connected`);
+  
+  io.sockets.emit('user joined', {
+    user: number
+  });
+  
+// Receive event emitted from client
   socket.on('chat', data => {
+    
+    //create current time using Date method
     var date = new Date();
     Chat.createdAt = date.toLocaleTimeString();
-    console.log(Chat.createdAt);
+    //console.log(Chat.createdAt);
 
     Chat.create({name: data.handle, message: data.message, createdAt: Chat.createdAt}).then((result) => {
-      console.log(result);
+      //console.log(result);
       io.sockets.emit('chat', {name: result.name, message: result.message, createdAt: result.createdAt}); // return data
     }).catch(err => console.error(err));
   });
@@ -52,5 +81,14 @@ io.on('connection', socket => {
   socket.on('typing', data => {
     socket.broadcast.emit('typing', data); // return data
   });
+
+  socket.on('disconnect', () => {
+    var count = --numUsers;
+    console.log(`${count} user(s) connected`);
+    io.sockets.emit('user left', {
+      user: count
+    });
+  });
 });
+
 
--- public/main.js
+++ public/main.js
@@ -8,54 +8,83 @@ const message = document.querySelector('#message'),
   chat = document.querySelector('#chat'),
   feedback = document.querySelector('#feedback'),
   clean = document.querySelector('#clean');
-time = document.querySelector('#time');
+  time = document.querySelector('#time');
+  userstatus = document.querySelector('#userstatus');
 
-// event
+//Add event for sending message on enter
+message.addEventListener("keyup", (e)=> {
+  e.preventDefault();
+  if(e.keyCode==13) {
+    btn.click();
+  }
+});
+
+//chat.style.overflow = 'hidden';
+chat.scrollTop = chat.scrollHeight;
+
 btn.addEventListener('click', () => {
   if (message.value != '' && handle.value != '') {
     handle.style.backgroundColor = "gray";
     handle.disabled = true;
-
+      
     socket.emit('chat', {
       message: message.value,
       handle: handle.value
     });
-
   } else {
     alert('All fields are required!');
   }
   message.value = '';
 });
 
+//Set background image
 function readURL(event) {
   var getImagePath = URL.createObjectURL(event.target.files[0]);
   chat.style.backgroundImage = 'url(' + getImagePath + ')';
 }
+
 
 message.addEventListener('keypress', () => {
   socket.emit('typing', handle.value);
 });
 
 socket.on('chat', data => {
-  console.log(data);
+  //console.log(data);
 
   chat.innerHTML += `<div class="msg">
         <div class="output" id="output">
         <strong>${data.name}: </strong>${data.message}   
           </div>
         <div class="time" id="time">
-        ${data.createdAt}
-        </div>
-        
+        <b>${data.createdAt}</b>
+        </div> 
     </div>`;
+    chat.scrollTop = chat.scrollHeight;
 });
 
 let timer = setTimeout(makeNoTypingState, 1000);
 
-socket.on('typing', data => {
+socket.on('typing', (data) => {
   feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
   clearTimeout(timer);
   timer = setTimeout(makeNoTypingState, 1000);
+});
+
+socket.on('user left', (data)=>{
+  if (data.user==1){
+  userstatus.innerHTML = `<p><em>Only <strong>YOU</strong> are <span>ONLINE</span> at the moment!</em></p>`;
+  }else {
+  userstatus.innerHTML = `<p><em><strong>${data.user}</strong> users <span>ONLINE</span> now</em></p>`;
+  }
+  
+});
+
+socket.on('user joined', (data)=>{
+  if (data.user==1){
+    userstatus.innerHTML = `<p><em>Only <strong>YOU</strong> are <span>ONLINE</span> at the moment!</em></p>`;
+    }else {
+    userstatus.innerHTML = `<p><em><strong>${data.user}</strong> users <span>ONLINE</span> now</em></p>`;
+    }
 });
 
 function makeNoTypingState() {
